74357a9aa16237330c90d93b72e6a572c05df58a
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-03-16 19:51:14 -0300

    feat: Ensure dependency files are created

--- Niffler.py
+++ Niffler.py
@@ -1,7 +1,37 @@
 #!/usr/bin/env python3
 
+from os import mkdir, environ, system
+from os.path import isdir, isfile
+
+
+# WARNING: Linux/Unix compatible only!
+def ensureDependencies() -> bool:
+    try:
+        HOME_PATH: str = environ['HOME']
+        INSTALL_PATH: str = HOME_PATH + "/.niffler/"
+        INDEX_PATH: str = INSTALL_PATH + "InvertedIndex.niffler"
+        IGNORE_PATH: str = INSTALL_PATH + "Ignore.niffler"
+
+        if not isdir(INSTALL_PATH):
+            mkdir(INSTALL_PATH)
+
+        if not isfile(INDEX_PATH):
+            system("touch " + INDEX_PATH)
+
+        if not isfile(IGNORE_PATH):
+            system("touch " + IGNORE_PATH)
+
+        return True
+    except:
+        return False
+
+
 def main() -> None:
-    print("Hello, World!")
+    if not ensureDependencies():
+        print("Error: Couldn't create nedded dependencies!")
+        exit(1)
+    else:
+        print("Ready! Set! Go!")
 
 
 if __name__ == "__main__":
