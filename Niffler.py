b8f54e49b72a192890d828f00ca9640ed886b742
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-03-17 19:16:27 -0300

    feat: Add supported arguments and validation

--- Niffler.py
+++ Niffler.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 
+from sys import argv
 from os import mkdir, environ, system
 from os.path import isdir, isfile
+
+Arguments = {
+    ("-a", "--add"): "Give a file (or some) to 'Niffler' and it will analyze it!",
+    ("-r", "--remove"): "Give a file to 'Niffler' and it will erase from his knowledge!",
+    ("-s", "--search-index"): "Give a term/word to 'Niffler' and he will find it's ocurrences on the Index!",
+    ("-S", "--show-index"): "Displays 'Niffler' immense knowledge, use with caution!",
+}
+
+ProgramName = argv.pop(0)
+
+
+def validateArgument(recievedArg: str) -> bool:
+    for keyPair, argDescription in Arguments.items():
+        if recievedArg in keyPair:
+            print(argDescription)
+            return True
+    return False
 
 
 # WARNING: Linux/Unix compatible only!
@@ -27,11 +45,13 @@ def ensureDependencies() -> bool:
 
 
 def main() -> None:
+
     if not ensureDependencies():
         print("Error: Couldn't create nedded dependencies!")
         exit(1)
-    else:
-        print("Ready! Set! Go!")
+
+    if len(argv) == 0 or not validateArgument(argv[0]):
+        print(f"Usage: {ProgramName} <OPTION> [FILES...]")
 
 
 if __name__ == "__main__":
