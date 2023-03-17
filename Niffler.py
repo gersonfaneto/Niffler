2d818a0b01bb1949913c9778309aad1279251354
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-03-17 20:27:28 -0300

    feat: Improve arguments validation

--- Niffler.py
+++ Niffler.py
@@ -5,19 +5,30 @@ from sys import argv
 from os import mkdir, environ, system
 from os.path import isdir, isfile
 
-Arguments: Dict[Tuple[str, str], str] = {
+SupportedArguments: Dict[Tuple[str, str], str] = {
     ("-a", "--add"): "Give a file (or some) to 'Niffler' and it will analyze it!",
     ("-r", "--remove"): "Give a file to 'Niffler' and it will erase from his knowledge!",
-    ("-s", "--search-index"): "Give a term/word to 'Niffler' and he will find it's ocurrences on the Index!",
+    ("-s", "--search-index"): "Give a term/word to 'Niffler' and he will find its ocurrences on the Index!",
     ("-S", "--show-index"): "Displays 'Niffler' immense knowledge, use with caution!",
 }
 
 ProgramName: str = argv.pop(0)
+Arguments: List[str] = [
+    x for x in argv if x.startswith("-") or x.startswith("--")
+]
 
 
-def validateArgument(recievedArg: str) -> bool:
-    for keyPair, argDescription in Arguments.items():
-        if recievedArg in keyPair:
+def validateArguments(recievedArgs: List[str]) -> bool:
+    if len(recievedArgs) == 0:
+        print("Error: 'Niffler' must recieve a command to execute!")
+        exit(1)
+
+    if len(recievedArgs) > 1:
+        print("Error: 'Niffler' can only run ONE operation at a time!")
+        exit(1)
+
+    for keyPair, argDescription in SupportedArguments.items():
+        if recievedArgs[0] in keyPair:
             print(argDescription)
             return True
     return False
@@ -46,12 +57,11 @@ def ensureDependencies() -> bool:
 
 
 def main() -> None:
-
     if not ensureDependencies():
         print("Error: Couldn't create nedded dependencies!")
         exit(1)
 
-    if len(argv) == 0 or not validateArgument(argv[0]):
+    if not validateArguments(Arguments):
         print(f"Usage: {ProgramName} <OPTION> [FILES...]")
 
 
