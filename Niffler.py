b5fd2a03a6431f051f531f0c229d7085a269366f
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-03-19 20:19:25 -0300

    feat: Integrate most of the functions with the CLI

--- Niffler.py
+++ Niffler.py
@@ -3,7 +3,7 @@
 from typing import Tuple, List, Dict
 from sys import argv
 from os import walk, mkdir, environ, system
-from os.path import isdir, isfile, join 
+from os.path import isdir, isfile, join, basename
 from string import punctuation
 
 
@@ -45,14 +45,26 @@ def helpMessage(programName: str, supportedOptions: Dict[Tuple[str, str], str], 
 
 
 def validateOption(supportedOptions: Dict[Tuple[str, str], str], recievedOption: str, hasComplement: bool = True) -> bool:
-    for keyPair, argDescription in supportedOptions.items():
+    for keyPair, optionDescription in supportedOptions.items():
         if recievedOption in keyPair and hasComplement:
             return True
         elif recievedOption in keyPair and not hasComplement:
-            print(f"You selected the '{recievedOption}' option! {argDescription}'")
+            print(f"You selected the '{recievedOption}' option! {optionDescription}")
             return False
 
     print(f"You selected the '{recievedOption}' option! 'Niffler' doesn't know this one...")
+    return False
+
+
+def validateModifiers(supportedModifiers: Dict[Tuple[str, str], str], recievedModifier: str, hasComplement: bool = True) -> bool:
+    for keyPair, _ in supportedModifiers.items():
+        if recievedModifier in keyPair and hasComplement:
+            return True
+        elif recievedModifier in keyPair and not hasComplement:
+            print(f"You used the '{recievedModifier}' modifier! By itself it doesn't do anything!")
+            return False
+
+    print(f"You used the '{recievedModifier} modifier! 'Niffler' doesn't know this one...")
     return False
 
 
@@ -76,30 +88,51 @@ def validatePaths(recievedPaths: List[str]) -> Tuple[List[str], List[str]]:
     return validPaths, invalidPaths
 
 
-def validateArguments(programName: str, supportedOptions: Dict[Tuple[str, str], str],
-                      supportedModifiers: Dict[Tuple[str, str], str], recievedOption: List[str],
-                      recievedPaths: List[str]) -> bool:
+def validateArguments(programName: str, supportedOptions: Dict[Tuple[str, str], str], supportedModifiers: Dict[Tuple[str, str], str],
+                      recievedOptions: List[str], recievedModifiers: List[str], recievedComplements: List[str]) -> bool:
+    if len(recievedOptions) == 0:
+        if len(recievedModifiers) == 0 and len(recievedComplements) == 0:
+            errorMessage(programName)
+            return False
+        elif len(recievedModifiers) == 1 and len(recievedComplements) == 0:
+            validateModifiers(supportedModifiers, recievedModifiers[0], False)
+            return False
+        else:
+            print("'Niffler' didn't detect any orders! Use the '--help' or '-h' flags for detailed usage information!")
+            return False
 
-    if len(recievedOption) == 0 and len(recievedPaths) == 0:
-        errorMessage(programName)
+    if len(recievedOptions) > 1:
+        print("'Niffler': Sorry! Can only handle ONE operation at a time!")
         return False
 
-    if len(recievedOption) == 0:
-        helpMessage(programName, supportedOptions, supportedModifiers)
-        return False
-
-    if len(recievedOption) == 1 and len(recievedPaths) == 0:
-        if recievedOption[0] in ["-h", "--help"]:
+    if len(recievedOptions) == 1:
+        if recievedOptions[0] in ["-h", "--help"]:
             helpMessage(programName, supportedOptions, supportedModifiers)
-            exit(0)
-        return validateOption(supportedOptions, recievedOption[0], hasComplement=False)
+            return True
 
-    if len(recievedOption) == 1 and len(recievedOption) > 0:
-        return validateOption(supportedOptions, recievedOption[0])
+        if len(recievedModifiers) == 1 and len(recievedComplements) == 0:
+            return validateOption(supportedOptions, recievedOptions[0], False)
 
-    if len(recievedOption) > 1:
-        print("ERROR: 'Niffler' can only run ONE operation at a time!")
-        return False
+        if len(recievedModifiers) == 1 and len(recievedComplements) > 0:
+            if validateModifiers(supportedModifiers, recievedModifiers[0]):
+                return validateOption(supportedOptions, recievedOptions[0])
+            else:
+                return False
+
+        if recievedOptions in ["-s", "--search-index"] and len(recievedComplements) == 1:
+            return True
+        if recievedOptions in ["-s", "--search-index"] and len(recievedComplements) == 0:
+            return validateOption(supportedOptions, recievedOptions[0], False)
+
+        if recievedOptions in ["-S", "--show-index"] and len(recievedComplements) == 0:
+            return True
+        if recievedOptions in ["-S", "--show-index"] and len(recievedComplements) > 0:
+            return validateOption(supportedOptions, recievedOptions[0], False)
+
+        if len(recievedComplements) > 0:
+            return validateOption(supportedOptions, recievedOptions[0])
+        else:
+            return validateOption(supportedOptions, recievedOptions[0], False)
 
     return True
 
@@ -113,7 +146,6 @@ def writeCache(cachePath: str, invertedIndex: Dict[str, Dict[str, int]]) -> None
     with open(cachePath, "w+") as indexCache:
         for word in invertedIndex.keys():
             indexCache.write(f"[{word}]:{invertedIndex[word]}\n")
-
 
 
 def readCache(cachePath: str, invertedIndex: Dict[str, Dict[str, int]]) -> None:
@@ -165,11 +197,12 @@ def main() -> None:
 
     homePath: str = environ['HOME']
     installPath: str = homePath + "/.niffler/"
-    invertedIndex_Cache: str = installPath + "InvertedIndex.niffler"
+    invertedIndexCache: str = installPath + "InvertedIndex.niffler"
 
     programName: str = argv.pop(0)
 
     supportedOptions: Dict[Tuple[str, str], str] = {
+        ("-h", "--help"): "'Niffler' shows you his abilities!",
         ("-a", "--add"): "Give a FILE (or some) to 'Niffler' and it will analyze it!",
         ("-r", "--remove"): "Give a FILE (or some) to 'Niffler' and it will be erased from his knowledge!",
         ("-s", "--search-index"): "Give a WORD to 'Niffler' and he will find its ocurrences on the Index!",
@@ -179,41 +212,52 @@ def main() -> None:
         ("-v", "--verbose"): "Extends the output information of some operations."
     }
 
-    recievedOptions: List[str] = [x for x in argv if x.startswith("-") or x.startswith("--")]
-    recievedPaths: List[str] = list(set(argv) - set(recievedOptions))
+    recievedOptions: List[str] = list(filter(lambda x: x in [y for z in supportedOptions.keys() for y in z], argv))
+    recievedModifiers: List[str] = list(filter(lambda x: x in [y for z in supportedModifiers.keys() for y in z], argv))
+    recievedComplements: List[str] = list(set(argv) - set(recievedOptions) - set(recievedModifiers))
 
     invertedIndex: Dict[str, Dict[str, int]] = {}
 
-    if not ensureDependencies(installPath, invertedIndex_Cache):
+    if not ensureDependencies(installPath, invertedIndexCache):
         print("'Niffler': Couldn't create nedded dependencies!")
         exit(1)
+    else:
+        readCache(invertedIndexCache, invertedIndex)
 
-    if not validateArguments(programName, supportedOptions, supportedModifiers, recievedOptions, recievedPaths):
+    if not validateArguments(programName, supportedOptions, supportedModifiers, recievedOptions, recievedModifiers, recievedComplements):
         exit(1)
 
-    validPaths, _ = validatePaths(recievedPaths)
-    chosenOption: str = recievedOptions[0]
+    validPaths, invalidPaths = validatePaths(recievedComplements)
+    chosenOption: str = recievedOptions[0] if len(recievedOptions) > 0 else ""
+    chosenModifier: str = recievedModifiers[0] if len(recievedModifiers) > 0 else ""
+    chosenWord: str = recievedComplements[0] if len(recievedComplements) > 0 else ""
 
-    # if len(invalidPaths) > 0:
-    #     print("\n'Niffler': The following files were out of reach!\n")
-    #     for currentPath in invalidPaths:
-    #         print(f"- {basename(currentPath)}")
-    #
-    # if len(validPaths) > 0:
-    #     print("\n'Niffler': Indexing the following files...\n")
-    #     for currentPath in validPaths:
-    #         print(f"- {basename(currentPath)}")
-    
-    if chosenOption in ["-a", "--add"]:
+    if chosenOption in ["-h", "--help"]:
+        pass
+    elif chosenOption in ["-a", "--add"]:
+        if chosenModifier in ["-v", "--verbose"]:
+            if len(invalidPaths) > 0:
+                print("\n'Niffler': The following files were out of reach!\n")
+                for currentPath in invalidPaths:
+                    print(f"- {basename(currentPath)}")
+            
+            if len(validPaths) > 0:
+                print("\n'Niffler': Indexing the following files...\n")
+                for currentPath in validPaths:
+                    print(f"- {basename(currentPath)}")
         for currentPath in validPaths:
             indexFile(currentPath, invertedIndex)
-        writeCache(invertedIndex_Cache, invertedIndex)
+    elif chosenOption in ["-r", "--remove"]:
+        pass
+    elif chosenOption in ["-s", "--search-index"]:
+        searchIndex(chosenWord, invertedIndex)
+    elif chosenOption in ["-S", "--show-index"]:
+        showIndex(invertedIndex)
     else:
-        print(f"'Niffler': Sorry! The option '{chosenOption}' is currently under development!")
-        exit(1)
+        print(f"'Niffler': Sorry! The option '{chosenOption}' is not implemented yet!")
 
-    searchIndex("Harry", invertedIndex)
-    
+    writeCache(invertedIndexCache, invertedIndex)
+
 
 if __name__ == "__main__":
     main()
